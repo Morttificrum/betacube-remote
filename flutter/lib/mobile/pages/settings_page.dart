24ac1fee12dd80e04b5edb29162bea59c76809e7
Morttificrum <morttificarum@hotmail.com>
2026-07-21 19:23:43 -0300

    Rebrand: Beta Cube Remote branding em pubspec.yaml, Cargo.toml e strings de interface (desktop/mobile) + traduções em 50 idiomas

--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -530,7 +530,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
               title: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(translate('Keep RustDesk background service')),
+                    Text(translate('Keep Beta Cube Remote background service')),
                     Text('* ${translate('Ignore Battery Optimizations')}',
                         style: Theme.of(context).textTheme.bodySmall),
                   ]),
@@ -1093,12 +1093,12 @@ void showThemeSettings(OverlayDialogManager dialogManager) async {
 void showAbout(OverlayDialogManager dialogManager) {
   dialogManager.show((setState, close, context) {
     return CustomAlertDialog(
-      title: Text(translate('About RustDesk')),
+      title: Text(translate('About Beta Cube Remote')),
       content: Wrap(direction: Axis.vertical, spacing: 12, children: [
         Text('Version: $version'),
         InkWell(
             onTap: () async {
-              const url = 'https://rustdesk.com/';
+              const url = 'https://betacube.com.br';
               await launchUrl(Uri.parse(url));
             },
             child: Padding(
